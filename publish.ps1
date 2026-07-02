f4f6c1e9033b3d21e94353fb89dd5254f63a670c
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-07-02 14:02:50 +0200

    Deploy independent docs manually

--- publish.ps1
+++ publish.ps1
@@ -3,6 +3,9 @@ param (
     [switch]$Publish
 )
 
+# Make sure the project is in sync:
+uv sync
+
 # Make sure the right Python is used:
 $env:QUARTO_PYTHON = "$PSScriptRoot\.venv\Scripts\python.exe"
 
@@ -14,20 +17,20 @@ if ($Render) {
     # Build all .qmd files:
     Get-ChildItem -Filter *.qmd -Recurse `
     | Where-Object { $_.FullName -notlike "*\.*" } | ForEach-Object {
+        # XXX do not use the following, as it will fail for the root!
+        # $relativeDir = $_.DirectoryName | Split-Path -Leaf
         $relativeDir = $_.DirectoryName.Substring($PSScriptRoot.Length).TrimStart('\')
         $targetDir = Join-Path "$PSScriptRoot\_site" $relativeDir
 
         Write-Host "Rendering $($_.FullName) to `n - $targetDir`n"
         quarto render $_.FullName --output-dir "$targetDir"
     }
-
-    # Ensure assets are available in the target directory:
-    Copy-Item -Recurse -Force "_assets" "$PSScriptRoot\_site"
 }
 
 if ($Publish) {
     # Force push the folder to your gh-pages branch
     cd "$PSScriptRoot\_site"
+    git config --global init.defaultBranch main
     git init
     git add -A
     git commit -m "Deploy independent docs manually"
