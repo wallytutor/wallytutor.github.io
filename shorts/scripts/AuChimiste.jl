0ed0a12077b50b9095c77adf0da36239637f7a71
Walter Dal'Maz Silva <walter.dalmazsilva@gmail.com>
2026-08-06 19:11:50 +0200

    Add plotting style

--- shorts/scripts/AuChimiste.jl
+++ shorts/scripts/AuChimiste.jl
@@ -3,6 +3,7 @@ module AuChimiste
 __init__() = nothing
 
 using Base
+using CairoMakie
 using DataFrames
 using Symbolics
 
@@ -13,6 +14,49 @@ import YAML
 
 export AuChimisteDatabase
 export Stoichiometry, MoleProportion, MassProportion, @component
+
+#region: plot-style
+const STYLE = Theme(
+    fontsize = 10,
+
+    # https://juliagraphics.github.io/Colors.jl/stable/namedcolors/
+    palette = (
+        color = [
+            # Start with classy black...
+            :black,
+
+            # Classic colors...
+            :red,
+            :blue,
+            :green,
+
+            # Degraded classics...
+            :tomato1,
+            :slateblue1,
+            :chartreuse1,
+        ],
+    ),
+
+    Axis = (
+        # Background:
+        backgroundcolor = :gray98,
+
+        # Grids:
+        xgridcolor = :gray20,
+        ygridcolor = :gray20,
+        xgridstyle = :dot,
+        ygridstyle = :dot,
+        xgridwidth = 0.8,
+        ygridwidth = 0.8,
+
+        # Spines:
+        leftspinevisible   = true,
+        rightspinevisible  = true,
+        bottomspinevisible = true,
+        topspinevisible    = true,
+    )
+)
+#endregion: plot-style
 
 #region: common
 function heaviside(T, T₀)
